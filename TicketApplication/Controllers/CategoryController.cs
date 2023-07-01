e412b5cc6914706464beffdcbd72a05d17e36dad
Borjan Obednikovski <obednikovskiborjan@gmail.com>
2023-07-02 01:34:39 +0200

    Second commit

---
+++ TicketApplication/Controllers/CategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketApplication.Data;
+using TicketApplication.Models;
+
+namespace TicketApplication.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> cateroryList = _context.Categories.ToList();
+            return View(cateroryList);
+        }
+
+        public IActionResult Create()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display Order can not match the name!");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+
+        }
+    }
+}
